81cc52a239a1d16bce9ccd2933e18609cf2d0534
u6305977 <u6305977@anu.edu.au>
2024-09-10 12:12:41 +1000

    Added ability to draw basic map

--- src/Game.java
+++ src/Game.java
@@ -10,6 +10,18 @@ public class Game {
 
     public void start() {
         // Main game "loop" - handle user inputs through Scanner
+
+        char[][] grid = new char[5][5];
+        grid[0][0] = '#'; grid[1][0] = '#'; grid[2][0] = '#'; grid[3][0]='#'; grid[4][0]='#';
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 1; j <= 3 ; j++) {
+                grid[i][j]='.';
+            }
+        }
+        grid[0][4] = '#'; grid[1][4] = '#'; grid[2][4] = '#'; grid[3][4]='#'; grid[4][4]='#';
+
+        Map map = new Map("map-default",grid);
+        map.draw();
     }
 
     private void handleMap() {
--- src/Map.java
+++ src/Map.java
@@ -4,29 +4,35 @@ public class Map {
 
     private String name;
     private final char[][] originalGrid = null;
-    private char[][] currentGrid = null;
+    private char[][] grid = null;
     private int width;
     private int height;
 
-    public Map(String name, String filePath) throws IOException {
+    public Map(String name, char[][] grid) {
         this.name = name;
-
+        this.grid = grid;
         // TODO: Read grid from file
         // TODO: Calculate width and height from file
-        this.width = -1;
-        this.height = -1;
+        this.width  = 5;
+        this.height = 5;
     }
 
     // Draw game map to terminal
     public void draw() {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                System.out.print(currentGrid[y][x]);
+                System.out.print(grid[x][y]);
             }
             System.out.println();
         }
     }
 
+    public char getTile(int x, int y) {
+        return grid[x][y];
+    }
 
+    public void setTile(int x, int y, char value) {
+        grid[x][y] = value;
+    }
 
 }
