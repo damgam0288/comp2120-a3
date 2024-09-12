6691ba2868c39f4c91584a02fca906889031014e
Damian Gamlath <u6305977@anu.edu.au>
2024-09-12 22:50:23 +1000

    Add dir for test resources

--- tests/MapTest.java
+++ tests/MapTest.java
@@ -1,8 +1,14 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class MapTest {
+
+    @Before
+
+
+
     @Test
     public void dummyTest() {
         assertEquals(5, 5);
