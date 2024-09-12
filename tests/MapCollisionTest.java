8114edca3431ffc2fa87d764160511d1abb82309
Damian Gamlath <u6305977@anu.edu.au>
2024-09-12 23:27:39 +1000

    Separate getCollidingEntity tests

---
+++ tests/MapCollisionTest.java
@@ -0,0 +1,48 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapCollisionTest {
+
+    private Player player;
+    private Map testMap;
+    private NPC collidingNPC;
+    private NPC nonCollidingNPC;
+
+    @Before
+    public void setup() throws Exception {
+        player = new Player(2,2,'P');
+
+        testMap = new Map("test-map-1","resources/test-map-1.json",player);
+
+        collidingNPC = new NPC(2,2, 'N');
+        nonCollidingNPC = new NPC(3,3,'N');
+        testMap.addEntity(collidingNPC);
+        testMap.addEntity(nonCollidingNPC);
+    }
+
+    @Test
+    public void testCorrectCollidingEntity() {
+        assertEquals(collidingNPC, testMap.getCollidingEntity());
+        assertNotEquals(nonCollidingNPC, testMap.getCollidingEntity());
+    }
+
+    @Test
+    public void testCorrectEntityAfterMove() {
+        // Colliding NPC no longer colliding with player
+        collidingNPC.move(1, 1, testMap);
+        assertNotEquals(collidingNPC, testMap.getCollidingEntity());
+
+        // Non-colliding NPC now colliding with player
+        nonCollidingNPC.move(-1,-1,testMap);
+        assertEquals(nonCollidingNPC, testMap.getCollidingEntity());
+    }
+
+    @Test
+    public void testNoCollision() {
+        // No collision at all
+        collidingNPC.move(1,1,testMap);
+        assertNull(testMap.getCollidingEntity());
+    }
+}
