a4e1de3de1685685816a1c952cd7646b2ec3a252
Damian Gamlath <u6305977@anu.edu.au>
2024-09-14 21:53:29 +1000

    Change Player parameters in MapCollisionTest with new constructor

--- tests/MapCollisionTest.java
+++ tests/MapCollisionTest.java
@@ -12,7 +12,7 @@ public class MapCollisionTest {
 
     @Before
     public void setup() throws Exception {
-        player = new Player(2,2,'P');
+        player = new Player(2,2,'P', 10, 100);
 
         testMap = new Map("test-map-1","resources/test-map-1.json",player);
 
