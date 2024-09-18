277e6cb93977b57e29bea746da35a9a36ad1ce75
Damian Gamlath <u6305977@anu.edu.au>
2024-09-18 15:56:40 +1000

    Create an ItemLoader to create items based on given JSON data

--- src/Item.java
+++ src/Item.java
@@ -1,26 +1,18 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 /**
  * Items can be held by Player or NPCs
  */
 
-public interface Item {
+public abstract class Item {
 
     String name = "";
-
-    public String getName();
-
-    public void setName(String name);
-
-}
-
-class Weapon implements Item {
-
-    private String name;
-    private int ap;     // Attack points provided by this weapon
-
-    public Weapon(String name, int ap) {
-        this.name = name;
-        this.ap = ap;
-    }
 
     public String getName() {
         return name;
@@ -28,6 +20,18 @@ class Weapon implements Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+}
+
+class Weapon extends Item {
+
+    private String name;
+    private int ap;     // Attack points provided by this weapon
+
+    public Weapon(String name, int ap) {
+        this.name = name;
+        this.ap = ap;
     }
 
     public int getAp() {
@@ -38,4 +42,39 @@ class Weapon implements Item {
         this.ap = ap;
     }
 
+}
+
+class ItemLoader {
+    /** Given a target item name and the file holding the data, this method will return
+     * an Item object with the written parameters */
+    public static Item loadItemFromFile(String target, String filepath) throws IOException {
+
+        // Read JSON file
+        String content = new String(Files.readAllBytes(Paths.get(filepath)));
+
+        // Search through all objects in the JSON file
+        JSONArray array = new JSONObject(content).getJSONArray("items");
+
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject ref = array.getJSONObject(i);
+
+            if (Objects.nonNull(ref)) {
+                String name = ref.getString("name");
+
+                // Found target
+                if (name.equalsIgnoreCase(target)) {
+                    if (ref.getString("type").equalsIgnoreCase("weapon")) {
+                        return new Weapon(ref.getString("name"),
+                                            ref.getInt("ap"));
+                    }
+
+//                    if (ref.getString("type").equalsIgnoreCase("shield")) {
+                        // TODO
+//                    }
+                }
+            }
+        }
+
+        return null;
+    }
 }
