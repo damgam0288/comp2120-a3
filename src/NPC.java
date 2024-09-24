ab31e53dc4809f70c4e554381b4f7ed6945df0d7
Damian Gamlath <u6305977@anu.edu.au>
2024-09-24 21:20:33 +1000

    Refactor NPC.java for interaction with Player to remove unnecessary item type checking (all handled by Inventory now)

--- src/NPC.java
+++ src/NPC.java
@@ -49,49 +49,29 @@ public class NPC extends Entity {
 
     public void giveItem(Player player) {
         player.receiveItem(item);
-        item=null;
+        item = null;
     }
 
     public boolean hasItem() {
         return (Objects.nonNull(item));
     }
 
-    public String getClue() {
-        return clue;
-    }
-
-    public void setClue(String clue) {
-        this.clue = clue;
-    }
-
     /**
      * Handles the interaction between the NPC and the player based on the current map.
+     * <p>
+     * If the npc has an item, it will give the item to the Player. Otherwise it will wish
+     * the Player well
      *
-     * If the player is on "map1" and has not yet received a weapon, the NPC gives the player a weapon
-     * that increases their attack points (AP). If the player has already received the weapon,
-     * the NPC gives a generic encouragement message. On "map2", the NPC provides a hint about the
-     * location of the exit.
-     *
-     * @param player The player interacting with the NPC.
-     * @param mapName The name of the current map where the interaction occurs.
+     * @param player  The player interacting with the NPC.
+     * @param mapName The name of the current map where the interaction occurs. todo no longer required field?
      */
     public void interact(Player player, String mapName) {
-        if (mapName.equals("map1")) {       // TODO: Incorporate this into the Game Config JSON file
-            if (hasItem()) {
-                System.out.println("NPC: Here's something to help!" + " ITEM RECEIVED: " + item.getName());
-
-                // TODO: Change this to simply add the item to inventory, when inventory is implemented
-                if (item.getClass().equals(Weapon.class)) {
-                    player.setAP(player.getAP() + 10);
-                    System.out.println("Your attack points (AP) is now: " + player.getAP());
-                }
-
-                this.giveItem(player);
-            } else {
-                System.out.println("NPC: Good luck out there!");
-            }
-        } else if (mapName.equals("map2")) {
-            System.out.println("NPC: The exit is to the south.");
+        if (hasItem()) {
+            System.out.println("NPC: Here's something to help! " + item.getName());
+            this.giveItem(player);
+        }
+        else {
+            System.out.println("NPC: Good luck out there!");
         }
     }
 }
